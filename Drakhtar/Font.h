d28c2c9acda09ddefa37107febf1e75e3f7596b6
Dankgard <dangar18@ucm.es>
2019-02-12 17:40:54 +0100

    Cambios menores en clases de Texto

--- Drakhtar/Font.h
+++ Drakhtar/Font.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "checkML.h"
 #include "SDL.h"
 #include "SDL_ttf.h"
 #include "Texture.h"
@@ -8,13 +7,13 @@
 class Font
 {
 private:
-	TTF_Font *_font = nullptr;
-	SDL_Renderer *_renderer;
+	TTF_Font* font = nullptr;
+	SDL_Renderer* renderer;
 	uint size = 8;
 
 public:
 	Font() {};
-	Font(SDL_Renderer *renderer, string filename, int size);
+	Font(SDL_Renderer* renderer, string filename, int size);
 	~Font();
 	TTF_Font* getFont() const;
 	SDL_Renderer* getRenderer() const;
