5e8c8207b9af87369cd015d9cee1f5897081863b
Dankgard <dangar18@ucm.es>
2019-02-13 11:27:40 +0100

    Terminada clase Font

--- Drakhtar/Font.cpp
+++ Drakhtar/Font.cpp
@@ -2,10 +2,33 @@
 
 
 
-Font::Font(SDL_Renderer* renderer, string filename, int size)
+Font::Font(SDL_Renderer* renderer, string filename, int size) : renderer(renderer), size(size)
 {
+	load(filename);
 }
 
 Font::~Font()
 {
+	liberate();
+}
+
+TTF_Font *Font::getFont() const
+{
+	return font;
+}
+
+SDL_Renderer *Font::getRenderer() const
+{
+	return renderer;
+}
+
+void Font::liberate()
+{
+	TTF_CloseFont(font);
+	font = nullptr;
+}
+
+void Font::load(string filename)
+{
+	font = TTF_OpenFont(filename.c_str(), size);
 }
