b9890a3ad5069484ab044012a3c771f13bef8c8e
Dankgard <dangar18@ucm.es>
2019-02-13 11:27:40 +0100

    Terminada clase Font

--- Drakhtar/Text.cpp
+++ Drakhtar/Text.cpp
@@ -1,6 +1,7 @@
 #include "Text.h"
 
 Text::Text(Font* font, int x, int y, int width, int height, SDL_Color &color, string &text)
+	:font(font), GameObject(x, y, width, height), color(color), text(text)
 {
 }
 
