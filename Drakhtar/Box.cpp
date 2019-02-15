4d81b9d10140bc3fd0b9744ae15955c42b47157f
onaranjoUCM <onaranjo@ucm.es>
2019-02-15 17:16:39 +0100

    Improved Board and Box

--- Drakhtar/Box.cpp
+++ Drakhtar/Box.cpp
@@ -9,9 +9,22 @@ Box::~Box() {
 	content = nullptr;
 }
 
+// Renders itself and its content
 void Box::render() const {
 	GameObject::render();
 	if (content != nullptr) {
 		content->render();
 	}
+}
+
+GameObject * Box::getContent() {
+	return content;
+}
+
+vector2d Box::getIndex() {
+	return { (float)boardX, (float)boardY };
+}
+
+void Box::setContent(GameObject* object) {
+	content = object;
 }
--- Drakhtar/Box.h
+++ Drakhtar/Box.h
@@ -4,10 +4,15 @@
 
 class Box : public GameObject {
 protected:
+	int boardX, boardY;
+	vector2d boardIndex;
 	GameObject* content;
 public:
 	Box(Texture* t, Vector2D<int> pos, Vector2D<int> size, GameObject* go);
 	virtual ~Box();
 
 	virtual void render() const;
+	GameObject* getContent();
+	vector2d getIndex();
+	void setContent(GameObject* object);
 };
