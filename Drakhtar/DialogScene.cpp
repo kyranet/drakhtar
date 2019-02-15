ec429e338dce92c9c165a3a1a5a682d3ea7693d2
Dankgard <dangar18@ucm.es>
2019-02-15 01:08:41 +0100

    Hechos métodos readFromFile y avanzando en constructores
    Aún falta lo relacionado con coger las texturas, fuente y color.

--- Drakhtar/DialogScene.cpp
+++ Drakhtar/DialogScene.cpp
@@ -2,18 +2,22 @@
 
 
 
-DialogScene::DialogScene(int dialogSize)
+DialogScene::DialogScene(Game* game, string filename)
 {
-	dialogChainSize = dialogSize;
+	//dialogBlockSprite = new GameObject(game->getTextures()[AÑADIR TEXTURA AL ENUM], vector2d{ 400, 400 }, vector2d{ 200, 80 });
+	readFromFile(game, filename);
 }
 
 
 DialogScene::~DialogScene()
 {
+	delete dialogBlockSprite;
+	dialogBlockSprite = nullptr;
 }
 
 void DialogScene::render()
 {
+	dialogBlockSprite->render();
 	dialogChain[currentDialogIndex]->render();
 }
 
@@ -29,8 +33,23 @@ void DialogScene::handleEvents(SDL_Event event)
 	}
 }
 
-void DialogScene::readFromFile()
+void DialogScene::readFromFile(Game* game, string filename)
 {
+	ifstream file;
+	file.open(filename + ".txt");
+	if (!file.is_open()) throw "No se encuentra el fichero";
+	else
+	{
+		file >> dialogChainSize;
+		dialogChain.reserve(dialogChainSize);
+
+		for (int i = 0;i < dialogChainSize;i++)
+		{
+			dialogChain[i] = new Dialog(game, file);
+		}
+		
+	}
+	file.close();
 }
 
 void DialogScene::nextDialog()
@@ -43,5 +62,6 @@ void DialogScene::nextDialog()
 
 void DialogScene::endOfDialog()
 {
+	// close dialog and go to next event
 }
 
