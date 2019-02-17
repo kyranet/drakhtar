8c64efc9229b743829f7c528b0208e0ea3ffdd5d
Dankgard <dangar18@ucm.es>
2019-02-17 12:48:02 +0100

    Añadida variable de color de texto en constructor de Dialog

--- Drakhtar/Dialog.cpp
+++ Drakhtar/Dialog.cpp
@@ -6,8 +6,17 @@ Dialog::Dialog(Game* game, ifstream& file)
 {
 	readFromFile(file);
 	characterPortraitSprite = new GameObject(game->getTextures[spriteText] , vector2d{ 220, 350 }, vector2d{ 70, 70 });
-	//characterNameSprite = new Text(COGER FUENTE, vector2d{ 220, 370 }, vector2d{ 50, 50 }, COGER COLOR, characterName);
-	//dialogTextSprite = new Text(COGER FUENTE, vector2d{ 400, 400 }, vector2d{ 50, 50 }, COGER COLOR, dialogText);
+
+	SDL_Color textColor;
+	textColor.r = 0;
+	textColor.g = 0;
+	textColor.b = 0;
+	textColor.a = 1;
+
+	//Font textFont
+
+	//characterNameSprite = new Text(COGER FUENTE, vector2d{ 220, 370 }, vector2d{ 50, 50 }, textColor, characterName);
+	//dialogTextSprite = new Text(COGER FUENTE, vector2d{ 400, 400 }, vector2d{ 50, 50 }, textColor, dialogText);
 }
 
 
