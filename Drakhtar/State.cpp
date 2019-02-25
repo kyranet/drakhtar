ffba62da83bedea2cbda05a5b682e759f82ef2e3
onaranjoUCM <onaranjo@ucm.es>
2019-02-25 17:47:06 +0100

    Resolve test

--- Drakhtar/State.cpp
+++ Drakhtar/State.cpp
@@ -12,7 +12,6 @@ State::~State()
 {
 	for (auto gameObject : gameObjects_)
 		delete gameObject;
-	delete exampleDialog_;
 	game_ = nullptr;
 }
 
@@ -24,7 +23,7 @@ void State::_preload()
 	// Board
 	Board * Tablero = new Board(TextureManager::get("UI-cellFrame"), 8, 12, 50);
 	gameObjects_.push_back(Tablero);
-	
+
 	// Test units
 	Box * box = Tablero->getBoxAt(0, 0);
 	Box * box2 = Tablero->getBoxAt(5, 5);
@@ -34,7 +33,8 @@ void State::_preload()
 	gameObjects_.push_back(test2);
 
 	// Dialog
-	exampleDialog_ = new DialogScene(game_, "dialog1_start", "Retron2000");
+	auto exampleDialog = new DialogScene(game_, "dialog1_start", "Retron2000");
+	gameObjects_.push_back(exampleDialog);
 }
 
 void State::run()
@@ -51,11 +51,22 @@ void State::run()
 	// → Run all the pending events of this tick from the stack
 	// → Destroy all the elements that are pending to destroy
 	// Once all tasks are done, exit loop, perform cleanup, and finish
+
+	uint lastTick = SDL_GetTicks();
+	uint elapsedTicks = 0;
+	uint requiredTicks = 1000 / ANIMATION_TICKS_PER_SECOND;
 	while (!_exit)
 	{
 		_create();
 		_handleEvents();
 		_update();
+
+		elapsedTicks = SDL_GetTicks() - lastTick;
+		if (elapsedTicks >= requiredTicks)
+		{
+			lastTick += elapsedTicks;
+			TextureManager::getInstance()->tick();
+		}
 		_afterUpdate();
 		_render();
 		_events();
@@ -66,7 +77,7 @@ void State::run()
 
 void State::_create()
 {
-	
+
 };
 void State::_render() const
 {
@@ -75,11 +86,7 @@ void State::_render() const
 
 	// Render each game object
 	for (auto gameObject : gameObjects_)
-	 	gameObject->render();
-
-	//exampleDialog_->render();
-
-	
+		gameObject->render();
 
 	// Render the new frame
 	SDL_RenderPresent(renderer_);
@@ -102,10 +109,44 @@ void State::_handleEvents()
 		// For each game object, run the event handler
 		for (auto gameObject : gameObjects_)
 			gameObject->handleEvents(event);
-
-		//exampleDialog_->handleEvents(event);
-
 	}
-};
-void State::_afterUpdate() {};
-void State::_events() {};
+}
+
+void State::_afterUpdate() {}
+
+void State::_events() {}
+
+void State::_destroy()
+{
+	for (auto gameObject : pendingOnDestroy_)
+	{
+		// If the gameObject was already deleted from memory,
+		// skip this search
+		if (gameObject == nullptr) continue;
+
+		auto it = gameObjects_.begin();
+		while (it != gameObjects_.end())
+		{
+			if (*it == gameObject)
+			{
+				gameObjects_.erase(it);
+				delete gameObject;
+				break;
+			}
+			it++;
+		}
+	}
+
+	pendingOnDestroy_.clear();
+}
+
+void State::addGameObject(GameObject* gameObject)
+{
+	gameObjects_.push_back(gameObject);
+}
+
+void State::removeGameObject(GameObject* gameObject)
+{
+	pendingOnDestroy_.push_back(gameObject);
+
+}
