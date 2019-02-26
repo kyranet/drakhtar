2d0897b6c3b201290cb30b211a38714c912fbcf2
MiguelZh <37276857+MiguelZh@users.noreply.github.com>
2019-02-26 17:15:14 +0100

    Added character name background sprite in dialogs

--- Drakhtar/TurnBar.h
+++ Drakhtar/TurnBar.h
@@ -4,15 +4,18 @@
 #include <list>
 #include "Unit.h"
 
-class TurnBar
+class TurnBar: public GameObject
 {
 private:
-	queue<Unit*> unitTurnBar;
+	list<Unit*> unitTurnBar;
 public:
 	// constructor intercalates units from each team list into the turn bar (ally -> enemy -> ally -> etc)
 	TurnBar(list<Unit*> allyList, list<Unit*> enemyList);
 	virtual ~TurnBar();
 	Unit* getFrontUnit() { return unitTurnBar.front(); }
 	void advanceTurn();
+	virtual void render();
+	virtual void handleEvents(SDL_Event event);
+
 };
 
