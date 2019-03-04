9232488c4535174b116c26e1b7fd9301d37842c7
AlejandroPla <aplazer@ucm.es>
2019-03-04 17:29:01 +0100

    Added example sprites to turnbar

--- Drakhtar/TurnBar.cpp
+++ Drakhtar/TurnBar.cpp
@@ -6,10 +6,16 @@ TurnBar::TurnBar() : GameObject(TextureManager::get("UI-turnBar"), Vector2D<int>
 {
 	// example for testing the turnBar
 	visibleUnits.resize(visibleTurnBarSize);
-	for (int i = 0;i < visibleTurnBarSize;i++)
-	{
-		visibleUnits[i] = new GameObject(TextureManager::get("Portraits-Thassa"), Vector2D<int>(WIN_WIDTH - 432 + (i + 1) * 44, WIN_HEIGHT - 30), Vector2D<int>(30, 30));
-	}
+	
+	visibleUnits[0] = new GameObject(TextureManager::get("Portraits-Thassa"), Vector2D<int>(WIN_WIDTH - 432 + 1 * 44, WIN_HEIGHT - 30), Vector2D<int>(30, 30));
+	visibleUnits[1] = new GameObject(TextureManager::get("Portraits-Archer"), Vector2D<int>(WIN_WIDTH - 432 + 2 * 44, WIN_HEIGHT - 30), Vector2D<int>(30, 30));
+	visibleUnits[2] = new GameObject(TextureManager::get("Portraits-ArcherEnemy"), Vector2D<int>(WIN_WIDTH - 432 + 3 * 44, WIN_HEIGHT - 30), Vector2D<int>(30, 30));
+	visibleUnits[3] = new GameObject(TextureManager::get("Portraits-Knight"), Vector2D<int>(WIN_WIDTH - 432 + 4 * 44, WIN_HEIGHT - 30), Vector2D<int>(30, 30));
+	visibleUnits[4] = new GameObject(TextureManager::get("Portraits-KnightEnemy"), Vector2D<int>(WIN_WIDTH - 432 + 5 * 44, WIN_HEIGHT - 30), Vector2D<int>(30, 30));
+	visibleUnits[5] = new GameObject(TextureManager::get("Portraits-Mage"), Vector2D<int>(WIN_WIDTH - 432 + 6 * 44, WIN_HEIGHT - 30), Vector2D<int>(30, 30));
+	visibleUnits[6] = new GameObject(TextureManager::get("Portraits-MageEnemy"), Vector2D<int>(WIN_WIDTH - 432 + 7 * 44, WIN_HEIGHT - 30), Vector2D<int>(30, 30));
+	visibleUnits[7] = new GameObject(TextureManager::get("Portraits-Narrator"), Vector2D<int>(WIN_WIDTH - 432 + 8 * 44, WIN_HEIGHT - 30), Vector2D<int>(30, 30));
+	visibleUnits[8] = new GameObject(TextureManager::get("Portraits-SoldierEnemy"), Vector2D<int>(WIN_WIDTH - 432 + 9 * 44, WIN_HEIGHT - 30), Vector2D<int>(30, 30));
 }
 
 TurnBar::TurnBar(list<Unit*> allyList, list<Unit*> enemyList): GameObject(TextureManager::get("UI-turnBar"), Vector2D<int>(100,100), Vector2D<int>(100, 100))
