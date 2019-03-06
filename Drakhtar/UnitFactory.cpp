f0c2e030048011212f5eb0b66d7afcf8660feaf8
Kyra <kyradiscord@gmail.com>
2019-03-06 14:44:11 +0100

    src: Requested changes, added RedMage to the TM, fixed bugs

--- Drakhtar/UnitFactory.cpp
+++ Drakhtar/UnitFactory.cpp
@@ -3,7 +3,7 @@
 
 UnitFactory::~UnitFactory() { }
 
-Unit * UnitFactory::newSoldier(Team * team, Box * box, int size)
+Unit* UnitFactory::newSoldier(Team* team, Box* box, int const& size)
 {
 	int soldierAttack = 5;
 	int soldierHealth = 10;
@@ -11,19 +11,16 @@ Unit * UnitFactory::newSoldier(Team * team, Box * box, int size)
 	int soldierMoveRange = 3;
 	int soldierSpeed = 3;
 
-	string textureName;
+	string textureName = team->getColor() == Color::BLUE
+		? "Units-BlueSoldier"
+		: "Units-RedSoldier";
 
-	if (team->getColor() == Color::BLUE)
-		textureName = "Units-BlueSoldier";
-	else
-		textureName = "Units-RedSoldier";
-
-	Unit* unit = (Unit*) new Battalion(TextureManager::get(textureName), box, soldierAttack, soldierHealth, soldierSpeed, soldierAttackRange, soldierMoveRange, size);
+	auto unit = new Battalion(TextureManager::get(textureName), box, soldierAttack, soldierHealth, soldierSpeed, soldierAttackRange, soldierMoveRange, size);
 	team->addUnit(unit);
 	return unit;
 }
 
-Unit * UnitFactory::newArcher(Team * team, Box * box, int size)
+Unit* UnitFactory::newArcher(Team* team, Box* box, int const& size)
 {
 	int archerAttack = 6;
 	int archerHealth = 10;
@@ -31,19 +28,16 @@ Unit * UnitFactory::newArcher(Team * team, Box * box, int size)
 	int archerMoveRange = 1;
 	int archerSpeed = 3;
 
-	string textureName;
+	string textureName = team->getColor() == Color::BLUE
+		? "Units-BlueArcher"
+		: "Units-RedArcher";
 
-	if (team->getColor() == Color::BLUE)
-		textureName = "Units-BlueArcher";
-	else
-		textureName = "Units-RedArcher";
-
-	Unit * unit = new Battalion(TextureManager::get(textureName), box, archerAttack, archerHealth, archerSpeed, archerAttackRange, archerMoveRange, size);
+	auto unit = new Battalion(TextureManager::get(textureName), box, archerAttack, archerHealth, archerSpeed, archerAttackRange, archerMoveRange, size);
 	team->addUnit(unit);
 	return unit;
 }
 
-Unit * UnitFactory::newKnight(Team * team, Box * box, int size)
+Unit* UnitFactory::newKnight(Team* team, Box* box, int const& size)
 {
 	int knightAttack = 8;
 	int knightHealth = 15;
@@ -51,19 +45,16 @@ Unit * UnitFactory::newKnight(Team * team, Box * box, int size)
 	int knightMoveRange = 5;
 	int knightSpeed = 5;
 
-	string textureName;
+	string textureName = team->getColor() == Color::BLUE
+		? "Units-BlueKnight"
+		: "Units-RedKnight";
 
-	if (team->getColor() == Color::BLUE)
-		textureName = "Units-BlueKnight";
-	else
-		textureName = "Units-RedKnight";
-
-	Unit * unit = new Battalion(TextureManager::get(textureName), box, knightAttack, knightHealth, knightSpeed, knightAttackRange, knightMoveRange, size);
+	auto unit = new Battalion(TextureManager::get(textureName), box, knightAttack, knightHealth, knightSpeed, knightAttackRange, knightMoveRange, size);
 	team->addUnit(unit);
 	return unit;
 }
 
-Unit * UnitFactory::newWizard(Team * team, Box * box, int size)
+Unit* UnitFactory::newWizard(Team* team, Box* box, int const& size)
 {
 	int wizardAttack = 4;
 	int wizardHealth = 7;
@@ -71,12 +62,16 @@ Unit * UnitFactory::newWizard(Team * team, Box * box, int size)
 	int wizardMoveRange = 1;
 	int wizardSpeed = 2;
 
-	Unit * unit = new Battalion(TextureManager::get("Units-BlueMage"), box, wizardAttack, wizardHealth, wizardSpeed, wizardAttackRange, wizardMoveRange, size);
+	string textureName = team->getColor() == Color::BLUE
+		? "Units-BlueMage"
+		: "Units-RedMage";
+
+	auto unit = new Battalion(TextureManager::get(textureName), box, wizardAttack, wizardHealth, wizardSpeed, wizardAttackRange, wizardMoveRange, size);
 	team->addUnit(unit);
 	return unit;
 }
 
-Unit * UnitFactory::newMonster(Team * team, Box * box, int size)
+Unit* UnitFactory::newMonster(Team* team, Box* box, int const& size)
 {
 	int monsterAttack = 9;
 	int monsterHealth = 15;
@@ -84,14 +79,11 @@ Unit * UnitFactory::newMonster(Team * team, Box * box, int size)
 	int monsterMoveRange = 5;
 	int monsterSpeed = 1;
 
-	string textureName;
+	string textureName = team->getColor() == Color::BLUE
+		? "Units-BlueMonster"
+		: "Units-RedMonster";
 
-	if (team->getColor() == Color::BLUE)
-		textureName = "Units-BlueMonster";
-	else
-		textureName = "Units-RedMonster";
-
-	Unit * unit =  new Battalion(TextureManager::get(textureName), box, monsterAttack, monsterHealth, monsterSpeed, monsterAttackRange, monsterMoveRange, size);
+	auto unit =  new Battalion(TextureManager::get(textureName), box, monsterAttack, monsterHealth, monsterSpeed, monsterAttackRange, monsterMoveRange, size);
 	team->addUnit(unit);
 	return unit;
 }
