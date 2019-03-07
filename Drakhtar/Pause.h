51511ff89da42be0fce7c296e4961ddcc7e913a9
GonzaPM7 <gonzap03@ucm.es>
2019-03-07 17:25:40 +0100

    Resolved the problem with Button

--- Drakhtar/Pause.h
+++ Drakhtar/Pause.h
@@ -7,16 +7,17 @@ class Pause :  public GameState
 {
 private:
 	SDL_Renderer* renderer_ = nullptr;
+	Game* game_ = nullptr;
 	Button* Resume;
 	Button* Options;
 	list<GameObject*> gameObjectsPause_;
 public:
-	Pause(SDL_Renderer* renderer);
+	Pause(Game* game,SDL_Renderer* renderer);
 	virtual ~Pause();
 	void _preload();
 	void _render();
 	void _handleEvents(SDL_Event& e);
-	static void resumeGame(SDL_Renderer* renderer);
-	static void optionsGame( SDL_Renderer* renderer);
+	static void resumeGame(Game * game, SDL_Renderer* renderer);
+	static void optionsGame(Game * game, SDL_Renderer* renderer);
 };
 
