f5cb01d5d6a665b1b8524b2602a5b59defcc5bd8
Dankgard <37511950+Dankgard@users.noreply.github.com>
2019-03-18 18:02:38 +0100

    feat: turnbar sorted by speed

--- Drakhtar/TurnBar.cpp
+++ Drakhtar/TurnBar.cpp
@@ -2,42 +2,6 @@
 
 #include "TurnBar.h"
 
-TurnBar::TurnBar()
-    : GameObject(TextureManager::get("UI-turnBar"),
-                 Vector2D<int>(WIN_WIDTH - 215, WIN_HEIGHT - 30),
-                 Vector2D<int>(400, 50))
-{
-    // example for testing the turnBar
-    visibleUnits.resize(visibleTurnBarSize);
-
-    visibleUnits[0] = new GameObject(TextureManager::get("Portraits-Thassa"),
-                                     Vector2D<int>(WIN_WIDTH - 432 + 1 * 44, WIN_HEIGHT - 30),
-                                     Vector2D<int>(30, 30));
-    visibleUnits[1] = new GameObject(TextureManager::get("Portraits-Archer"),
-                                     Vector2D<int>(WIN_WIDTH - 432 + 2 * 44, WIN_HEIGHT - 30),
-                                     Vector2D<int>(30, 30));
-    visibleUnits[2] = new GameObject(TextureManager::get("Portraits-ArcherEnemy"),
-                                     Vector2D<int>(WIN_WIDTH - 432 + 3 * 44, WIN_HEIGHT - 30),
-                                     Vector2D<int>(30, 30));
-    visibleUnits[3] = new GameObject(TextureManager::get("Portraits-Knight"),
-                                     Vector2D<int>(WIN_WIDTH - 432 + 4 * 44, WIN_HEIGHT - 30),
-                                     Vector2D<int>(30, 30));
-    visibleUnits[4] = new GameObject(TextureManager::get("Portraits-KnightEnemy"),
-                                     Vector2D<int>(WIN_WIDTH - 432 + 5 * 44, WIN_HEIGHT - 30),
-                                     Vector2D<int>(30, 30));
-    visibleUnits[5] = new GameObject(TextureManager::get("Portraits-Mage"),
-                                     Vector2D<int>(WIN_WIDTH - 432 + 6 * 44, WIN_HEIGHT - 30),
-                                     Vector2D<int>(30, 30));
-    visibleUnits[6] = new GameObject(TextureManager::get("Portraits-MageEnemy"),
-                                     Vector2D<int>(WIN_WIDTH - 432 + 7 * 44, WIN_HEIGHT - 30),
-                                     Vector2D<int>(30, 30));
-    visibleUnits[7] = new GameObject(TextureManager::get("Portraits-Narrator"),
-                                     Vector2D<int>(WIN_WIDTH - 432 + 8 * 44, WIN_HEIGHT - 30),
-                                     Vector2D<int>(30, 30));
-    visibleUnits[8] = new GameObject(TextureManager::get("Portraits-SoldierEnemy"),
-                                     Vector2D<int>(WIN_WIDTH - 432 + 9 * 44, WIN_HEIGHT - 30),
-                                     Vector2D<int>(30, 30));
-}
 
 TurnBar::TurnBar(list<Unit *> allyList, list<Unit *> enemyList)
     : GameObject(TextureManager::get("UI-turnBar"),
@@ -72,6 +36,14 @@ TurnBar::TurnBar(list<Unit *> allyList, list<Unit *> enemyList)
                                          Vector2D<int>(45, 45));
         listIt++;
     }
+	// example for testing sort
+	//auto e = allyList.begin();
+	//unitTurnBar.push_back(*e);
+	//unitTurnBar.push_back(*e);
+	//unitTurnBar.push_back(*e);
+	//unitTurnBar.push_back(*e);
+	//unitTurnBar.push_back(*e);
+	sort();
 }
 
 TurnBar::~TurnBar()
@@ -87,6 +59,12 @@ void TurnBar::advanceTurn()
     unitTurnBar.pop_front();
     unitTurnBar.push_back(frontUnit);
     updateVisibleUnits();
+}
+
+void TurnBar::sort()
+{
+	unitTurnBar.sort([](Unit * unit1, Unit * unit2) { return unit1->getSpeed() > unit2->getSpeed(); });
+	updateVisibleUnits();
 }
 
 void TurnBar::render() const
--- Drakhtar/TurnBar.h
+++ Drakhtar/TurnBar.h
@@ -15,12 +15,12 @@ class TurnBar : public GameObject
     vector<GameObject *> visibleUnits;
 
  public:
-    TurnBar();
     // constructor intercalates units from each team list into the turn bar (ally -> enemy -> ally -> etc)
     TurnBar(list<Unit *> allyList, list<Unit *> enemyList);
     virtual ~TurnBar();
     Unit *getFrontUnit() { return unitTurnBar.front(); }
     void advanceTurn();
+	void sort();
     virtual void render() const;
     virtual void handleEvents(SDL_Event event);
     void updateVisibleUnits();
