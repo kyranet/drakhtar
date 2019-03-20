b263114f416fba2d762852402f7b6fc73e483278
onaranjoUCM <onaranjo@ucm.es>
2019-03-20 17:28:53 +0100

    Revert "fix: Clang formatting"
    This reverts commit c6d7067a89e305e94309613f07ec3ded3a98cb5b.

--- Drakhtar/Board.h
+++ Drakhtar/Board.h
@@ -2,49 +2,55 @@
 
 #pragma once
 
+#include "GameObject.h"
 #include "Box.h"
 #include "Constants.h"
-#include "GameObject.h"
 #include "Matrix.h"
 
-class Board : public GameObject {
+class Board : public GameObject
+{
  protected:
-  int rows, cols;
-  float marginX, marginY, cellSize;
-  Box ***board;
-  Matrix<int> *cellsMatrix = nullptr;
+    int rows, cols;
+    float marginX, marginY, cellSize;
+    Box ***board;
+    Matrix<int> *cellsMatrix = nullptr;
 
  public:
-  Board(Texture *cellTexture, int r, int c, float cellSize);
-  virtual ~Board();
+    Board(Texture *cellTexture, int r, int c, float cellSize);
+    virtual ~Board();
 
-  virtual void render() const;
-  virtual void handleEvents(SDL_Event event);
+    virtual void render() const;
+    virtual void handleEvents(SDL_Event event);
 
-  // Returns cell in board index (x, y)
-  Box *getBoxAt(int x, int y);
+	// Returns cell in board index (x, y)
+    Box *getBoxAt(int x, int y);
 
-  // Returns cell in window coordinates (x, y)
-  Box *getBoxAtCoordinates(Vector2D<int> coordinates);
+	// Returns cell in window coordinates (x, y)
+    Box *getBoxAtCoordinates(Vector2D<int> coordinates);
 
-  // Checks if two cells are within a certain range of each other
-  bool isInRange(Box *from, Box *to, int range);
+	// Checks if two cells are within a certain range of each other
+    bool isInRange(Box *from, Box *to, int range);
 
-  // Returns an integer matrix of the contents of cells in range of a specific
-  // cell
-  Matrix<int> *getCellsInRange(Box *box, int range);
+	// Returns an integer matrix of the contents of cells in range of a specific cell
+    Matrix<int> *getCellsInRange(Box *box, int range);
 
-  // Checks is there is one or more enemies in range
-  bool isEnemyInRange(Box *box, int range);
+	// Checks is there is one or more enemies in range
+    bool isEnemyInRange(Box *box, int range);
 
-  // Changes texture of empty cells in range
-  void highlightCellsInRange(Box *box, int range);
+	// Changes texture of empty cells in range
+    void highlightCellsInRange(Box *box, int range);
 
-  // Changes texture of cells with enemies in range
-  void highlightEnemiesInRange(Box *box, int range);
+	// Changes texture of cells with enemies in range
+    void highlightEnemiesInRange(Box *box, int range);
 
-  // Resets ALL cells to their base texture
-  void resetCellsToBase();
+	// Resets ALL cells to their base texture
+    void resetCellsToBase();
 
-  const enum objectType { outOfBoard, outOfRange, empty, ally, enemy };
+    const enum objectType {
+        outOfBoard,
+        outOfRange,
+        empty,
+        ally,
+        enemy
+    };
 };
--- Drakhtar/Game.cpp
+++ Drakhtar/Game.cpp
@@ -3,119 +3,91 @@
 #include "Game.h"
 #include "SDLError.h"
 
-Game::Game() : window_(nullptr), renderer_(nullptr) {
-  if (SDL_Init(SDL_INIT_EVENTS | SDL_INIT_TIMER | SDL_INIT_AUDIO) != 0) {
-    string message =
-        string("Error initializing SDL.\nReason: ") + SDL_GetError();
-    throw new SDLError(message);
-  }
+Game::Game()
+    : window_(nullptr), renderer_(nullptr)
+{
+    if (SDL_Init(SDL_INIT_EVENTS | SDL_INIT_TIMER | SDL_INIT_AUDIO) != 0)
+    {
+        string message = string("Error initializing SDL.\nReason: ") + SDL_GetError();
+        throw new SDLError(message);
+    }
 
-  if (TTF_Init() != 0) {
-    string message =
-        string("Error initializing TTF.\nReason: ") + TTF_GetError();
-    throw new SDLError(message);
-  }
+    if (TTF_Init() != 0)
+    {
+        string message = string("Error initializing TTF.\nReason: ") + TTF_GetError();
+        throw new SDLError(message);
+    }
 
-  if (Mix_Init(MIX_INIT_MP3 | MIX_INIT_OGG) == 0) {
-    string message =
-        string("Error initializing MIX.\nReason: ") + Mix_GetError();
-    throw new SDLError(message);
-  }
+    if (Mix_Init(MIX_INIT_MP3 | MIX_INIT_OGG) == 0)
+    {
+        string message = string("Error initializing MIX.\nReason: ") + Mix_GetError();
+        throw new SDLError(message);
+    }
 
-  // Units
-  textureManager_->add("Units-BlueArcher", "../images/Units/BlueArcher.png", 2,
-                       1, SDL_FLIP_HORIZONTAL);
-  textureManager_->add("Units-BlueKnight", "../images/Units/BlueKnight.png", 2,
-                       1, SDL_FLIP_HORIZONTAL);
-  textureManager_->add("Units-BlueMage", "../images/Units/BlueMage.png", 2, 1,
-                       SDL_FLIP_HORIZONTAL);
-  textureManager_->add("Units-BlueMonster", "../images/Units/BlueMonster.png",
-                       2, 1, SDL_FLIP_HORIZONTAL);
-  textureManager_->add("Units-BlueSoldier", "../images/Units/BlueSoldier.png",
-                       2, 1, SDL_FLIP_HORIZONTAL);
-  textureManager_->add("Units-BlueValar", "../images/Units/BlueValar.png", 2, 1,
-                       SDL_FLIP_HORIZONTAL);
-  textureManager_->add("Units-Thassa", "../images/Units/Thassa.png", 2, 1,
-                       SDL_FLIP_HORIZONTAL);
-  textureManager_->add("Units-Abeizhul", "../images/Units/Abeizhul.png", 2, 1);
-  textureManager_->add("Units-Dreilay", "../images/Units/Dreilay.png", 2, 1);
-  textureManager_->add("Units-RedArcher", "../images/Units/RedArcher.png", 2,
-                       1);
-  textureManager_->add("Units-RedKnight", "../images/Units/RedKnight.png", 2,
-                       1);
-  textureManager_->add("Units-RedMage", "../images/Units/RedMage.png", 2, 1);
-  textureManager_->add("Units-RedMonster", "../images/Units/RedMonster.png", 2,
-                       1);
-  textureManager_->add("Units-RedSoldier", "../images/Units/RedSoldier.png", 2,
-                       1);
-  textureManager_->add("Units-Sheissa", "../images/Units/Sheissa.png", 2, 1);
-  textureManager_->add("Units-Valar", "../images/Units/Valar.png", 2, 1);
-  textureManager_->add("Units-Zamdran", "../images/Units/Zamdran.png", 2, 1);
+    // Units
+    textureManager_->add("Units-BlueArcher", "../images/Units/BlueArcher.png", 2, 1, SDL_FLIP_HORIZONTAL);
+    textureManager_->add("Units-BlueKnight", "../images/Units/BlueKnight.png", 2, 1, SDL_FLIP_HORIZONTAL);
+    textureManager_->add("Units-BlueMage", "../images/Units/BlueMage.png", 2, 1, SDL_FLIP_HORIZONTAL);
+    textureManager_->add("Units-BlueMonster", "../images/Units/BlueMonster.png", 2, 1, SDL_FLIP_HORIZONTAL);
+    textureManager_->add("Units-BlueSoldier", "../images/Units/BlueSoldier.png", 2, 1, SDL_FLIP_HORIZONTAL);
+    textureManager_->add("Units-BlueValar", "../images/Units/BlueValar.png", 2, 1, SDL_FLIP_HORIZONTAL);
+    textureManager_->add("Units-Thassa", "../images/Units/Thassa.png", 2, 1, SDL_FLIP_HORIZONTAL);
+    textureManager_->add("Units-Abeizhul", "../images/Units/Abeizhul.png", 2, 1);
+    textureManager_->add("Units-Dreilay", "../images/Units/Dreilay.png", 2, 1);
+    textureManager_->add("Units-RedArcher", "../images/Units/RedArcher.png", 2, 1);
+    textureManager_->add("Units-RedKnight", "../images/Units/RedKnight.png", 2, 1);
+    textureManager_->add("Units-RedMage", "../images/Units/RedMage.png", 2, 1);
+    textureManager_->add("Units-RedMonster", "../images/Units/RedMonster.png", 2, 1);
+    textureManager_->add("Units-RedSoldier", "../images/Units/RedSoldier.png", 2, 1);
+    textureManager_->add("Units-Sheissa", "../images/Units/Sheissa.png", 2, 1);
+    textureManager_->add("Units-Valar", "../images/Units/Valar.png", 2, 1);
+    textureManager_->add("Units-Zamdran", "../images/Units/Zamdran.png", 2, 1);
 
-  // UI
-  textureManager_->add("UI-cellFrame", "../images/UI/cellFrame.png", 1, 1);
-  textureManager_->add("UI-cellHover", "../images/UI/cellHover.png", 1, 1);
-  textureManager_->add("UI-cellInRange", "../images/UI/cellInRange.png", 1, 1);
-  textureManager_->add("UI-enemyInRange", "../images/UI/enemyInRange.png", 1,
-                       1);
-  textureManager_->add("UI-activeUnit", "../images/UI/activeUnitCell.png", 1,
-                       1);
-  textureManager_->add("UI-dialogueBackground",
-                       "../images/UI/dialogueBackground.png", 1, 1);
-  textureManager_->add("UI-turnBar", "../images/UI/turnBar.png", 1, 1);
+    // UI
+    textureManager_->add("UI-cellFrame", "../images/UI/cellFrame.png", 1, 1);
+    textureManager_->add("UI-cellHover", "../images/UI/cellHover.png", 1, 1);
+    textureManager_->add("UI-cellInRange", "../images/UI/cellInRange.png", 1, 1);
+    textureManager_->add("UI-enemyInRange", "../images/UI/enemyInRange.png", 1, 1);
+	textureManager_->add("UI-activeUnit", "../images/UI/activeUnitCell.png", 1, 1);
+    textureManager_->add("UI-dialogueBackground", "../images/UI/dialogueBackground.png", 1, 1);
+    textureManager_->add("UI-turnBar", "../images/UI/turnBar.png", 1, 1);
 
-  // Button
-  textureManager_->add("Button-Play", "../images/MainMenu/Play_Button.png", 1,
-                       1);
-  textureManager_->add("Button-Options",
-                       "../images/MainMenu/Options_Button.png", 1, 1);
-  textureManager_->add("Button-Pause", "../images/Pause/Pause_Button.png", 1,
-                       1);
-  textureManager_->add("Button-Resume", "../images/Pause/Resume_Button.png", 1,
-                       1);
-  textureManager_->add("Button-Restart", "../images/Pause/Restart_Button.png",
-                       1, 1);
-  textureManager_->add("Button-Exit", "../images/Pause/Exit_Button.png", 1, 1);
+    // Button
+    textureManager_->add("Button-Play", "../images/MainMenu/Play_Button.png", 1, 1);
+    textureManager_->add("Button-Options", "../images/MainMenu/Options_Button.png", 1, 1);
+    textureManager_->add("Button-Pause", "../images/Pause/Pause_Button.png", 1, 1);
+    textureManager_->add("Button-Resume", "../images/Pause/Resume_Button.png", 1, 1);
+    textureManager_->add("Button-Restart", "../images/Pause/Restart_Button.png", 1, 1);
+    textureManager_->add("Button-Exit", "../images/Pause/Exit_Button.png", 1, 1);
 
-  // Pause
-  textureManager_->add("Pause-Panel", "../images/Pause/Panel.png", 1, 1);
-  textureManager_->add("Pause-Background", "../images/Pause/Fondo.png", 1, 1);
+    // Pause
+    textureManager_->add("Pause-Panel", "../images/Pause/Panel.png", 1, 1);
+    textureManager_->add("Pause-Background", "../images/Pause/Fondo.png", 1, 1);
 
-  // Portraits
-  textureManager_->add("Portraits-Archer", "../images/Portraits/Archer.png", 1,
-                       1);
-  textureManager_->add("Portraits-ArcherEnemy",
-                       "../images/Portraits/ArcherEnemy.png", 1, 1);
-  // TODO(Javi): Nuke this
-  textureManager_->add("Portraits-characterPlaceholder",
-                       "../images/Portraits/characterPlaceholder.png", 1, 1);
-  textureManager_->add("Portraits-Knight", "../images/Portraits/Knight.png", 1,
-                       1);
-  textureManager_->add("Portraits-KnightEnemy",
-                       "../images/Portraits/KnightEnemy.png", 1, 1);
-  textureManager_->add("Portraits-Mage", "../images/Portraits/Mage.png", 1, 1);
-  textureManager_->add("Portraits-MageEnemy",
-                       "../images/Portraits/MageEnemy.png", 1, 1);
-  textureManager_->add("Portraits-Narrator", "../images/Portraits/Narrator.png",
-                       1, 1);
-  textureManager_->add("Portraits-SoldierEnemy",
-                       "../images/Portraits/SoldierEnemy.png", 1, 1);
-  textureManager_
-      ->add("Portraits-Thassa", "../images/Portraits/Thassa.png", 3, 3)
-      ->addAnimation("default", {0})
-      ->addAnimation("angry", {1})
-      ->addAnimation("pray", {2})
-      ->addAnimation("courage", {3})
-      ->addAnimation("shout", {4})
-      ->addAnimation("surprise", {5})
-      ->addAnimation("sad", {6});
+    // Portraits
+    textureManager_->add("Portraits-Archer", "../images/Portraits/Archer.png", 1, 1);
+    textureManager_->add("Portraits-ArcherEnemy", "../images/Portraits/ArcherEnemy.png", 1, 1);
+    // TODO(Javi): Nuke this
+    textureManager_->add("Portraits-characterPlaceholder", "../images/Portraits/characterPlaceholder.png", 1, 1);
+    textureManager_->add("Portraits-Knight", "../images/Portraits/Knight.png", 1, 1);
+    textureManager_->add("Portraits-KnightEnemy", "../images/Portraits/KnightEnemy.png", 1, 1);
+    textureManager_->add("Portraits-Mage", "../images/Portraits/Mage.png", 1, 1);
+    textureManager_->add("Portraits-MageEnemy", "../images/Portraits/MageEnemy.png", 1, 1);
+    textureManager_->add("Portraits-Narrator", "../images/Portraits/Narrator.png", 1, 1);
+    textureManager_->add("Portraits-SoldierEnemy", "../images/Portraits/SoldierEnemy.png", 1, 1);
+    textureManager_->add("Portraits-Thassa", "../images/Portraits/Thassa.png", 3, 3)
+        ->addAnimation("default", {0})
+        ->addAnimation("angry", {1})
+        ->addAnimation("pray", {2})
+        ->addAnimation("courage", {3})
+        ->addAnimation("shout", {4})
+        ->addAnimation("surprise", {5})
+        ->addAnimation("sad", {6});
 
-  // Maps
-  textureManager_->add("Maps-Test", "../images/Maps/TestMap.png", 1, 1);
-  textureManager_->add("Maps-FirstBattle", "../images/Maps/FirstBattle.png", 1,
-                       1);
-  textureManager_->add("Maps-SecondBattle", "../images/Maps/SecondBattle.png",
-                       1, 1);
+    // Maps
+    textureManager_->add("Maps-Test", "../images/Maps/TestMap.png", 1, 1);
+    textureManager_->add("Maps-FirstBattle", "../images/Maps/FirstBattle.png", 1, 1);
+    textureManager_->add("Maps-SecondBattle", "../images/Maps/SecondBattle.png", 1, 1);
 
       // Fonts
       fontManager_->add("Retron2000", "../fonts/Retron2000.ttf", 12, WIN_WIDTH);
@@ -134,75 +106,90 @@ Game::Game() : window_(nullptr), renderer_(nullptr) {
   if (window_ == nullptr || renderer_ == nullptr)
     throw new SDLError("Error loading the SDL window or renderer");
 
-  stateMachine = new GameStateMachine();
-  state_ = new MainMenu(this, renderer_);
-  stateMachine->pushState(state_);
+    stateMachine = new GameStateMachine();
+    state_ = new MainMenu(this, renderer_);
+    stateMachine->pushState(state_);
 }
 
-void Game::run() {
-  while (!stateMachine->getCurrentState()->getexit()) {
-    // Preload the state before running
-    currentState_ = stateMachine->getCurrentState();
-    stateMachine->getCurrentState()->_preload();
+void Game::run()
+{
+    while (!stateMachine->getCurrentState()->getexit())
+    {
+        // Preload the state before running
+        currentState_ = stateMachine->getCurrentState();
+        stateMachine->getCurrentState()->_preload();
 
-    // The event loop follows this scheme:
-    // → Create all pending-to-create game objects
-    // → Handle SDL events (provided by SDL's event poll)
-    // → Handle updates (updates all game objects of the game)
-    // → Handle after updates (called after all the updates have run)
-    // → Render all the game objects from the scene
-    // → Run all the pending events of this tick from the stack
-    // → Destroy all the elements that are pending to destroy
-    // Once all tasks are done, exit loop, perform cleanup, and finish
+        // The event loop follows this scheme:
+        // → Create all pending-to-create game objects
+        // → Handle SDL events (provided by SDL's event poll)
+        // → Handle updates (updates all game objects of the game)
+        // → Handle after updates (called after all the updates have run)
+        // → Render all the game objects from the scene
+        // → Run all the pending events of this tick from the stack
+        // → Destroy all the elements that are pending to destroy
+        // Once all tasks are done, exit loop, perform cleanup, and finish
 
-    uint lastTick = SDL_GetTicks();
-    uint elapsedTicks = 0;
-    uint requiredTicks = 1000 / ANIMATION_TICKS_PER_SECOND;
-    while (stateMachine->getCurrentState() == currentState_ &&
-           !stateMachine->getCurrentState()->getexit()) {
-      stateMachine->getCurrentState()->_create();
-      stateMachine->getCurrentState()->_handleEvents(event);
-      stateMachine->getCurrentState()->_update();
+        uint lastTick = SDL_GetTicks();
+        uint elapsedTicks = 0;
+        uint requiredTicks = 1000 / ANIMATION_TICKS_PER_SECOND;
+        while (stateMachine->getCurrentState() == currentState_ && !stateMachine->getCurrentState()->getexit())
+        {
+            stateMachine->getCurrentState()->_create();
+            stateMachine->getCurrentState()->_handleEvents(event);
+            stateMachine->getCurrentState()->_update();
 
-      elapsedTicks = SDL_GetTicks() - lastTick;
-      if (elapsedTicks >= requiredTicks) {
-        lastTick += elapsedTicks;
-        TextureManager::getInstance()->tick();
-      }
-      stateMachine->getCurrentState()->_afterUpdate();
-      stateMachine->getCurrentState()->_render();
-      stateMachine->getCurrentState()->_events();
-      stateMachine->getCurrentState()->_destroy();
+            elapsedTicks = SDL_GetTicks() - lastTick;
+            if (elapsedTicks >= requiredTicks)
+            {
+                lastTick += elapsedTicks;
+                TextureManager::getInstance()->tick();
+            }
+            stateMachine->getCurrentState()->_afterUpdate();
+            stateMachine->getCurrentState()->_render();
+            stateMachine->getCurrentState()->_events();
+            stateMachine->getCurrentState()->_destroy();
+        }
+        stateMachine->getCurrentState()->_end();
     }
-    stateMachine->getCurrentState()->_end();
-  }
 }
 
-Game::~Game() {
-  SDL_DestroyRenderer(renderer_);
-  SDL_DestroyWindow(window_);
+Game::~Game()
+{
+    SDL_DestroyRenderer(renderer_);
+    SDL_DestroyWindow(window_);
 
     delete stateMachine;
     FontManager::destroy();
 
-  SDL_Quit();
-  TTF_Quit();
-  Mix_Quit();
+    SDL_Quit();
+    TTF_Quit();
+    Mix_Quit();
 }
 
-SDL_Renderer *Game::getRenderer() { return renderer_; }
+SDL_Renderer *Game::getRenderer()
+{
+    return renderer_;
+}
 
 Game *Game::instance = nullptr;
 
-Game *Game::getInstance() {
-  if (instance == nullptr) {
-    instance = new Game();
-  }
+Game *Game::getInstance()
+{
+    if (instance == nullptr)
+    {
+        instance = new Game();
+    }
 
-  return instance;
+    return instance;
 }
 
-GameStateMachine *Game::getStateMachine() { return stateMachine; }
+GameStateMachine *Game::getStateMachine()
+{
+    return stateMachine;
+}
 
-GameState *Game::currentState() { return getInstance()->currentState_; }
+GameState *Game::currentState()
+{
+    return getInstance()->currentState_;
+}
 
