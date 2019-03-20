5fc7bde5ada355775e8f088b792e8c8c0f8f485f
onaranjoUCM <onaranjo@ucm.es>
2019-03-20 17:28:53 +0100

    fix: Clang formatting

--- Drakhtar/Box.cpp
+++ Drakhtar/Box.cpp
@@ -1,77 +1,51 @@
 // Copyright 2019 the Drakhtar authors. All rights reserved. MIT license.
 
 #include "Box.h"
-#include "Unit.h"
 #include "TextureManager.h"
+#include "Unit.h"
 
-Box::Box(Texture *t, Vector2D<int> pos, Vector2D<int> size, Vector2D<int> bi, Unit *go)
-    : GameObject(t, pos, size), boardIndex(bi), content(go)
-{
-    cellTextures[base] = t;
-    cellTextures[hover] = TextureManager::get("UI-cellHover");
-    cellTextures[movable] = TextureManager::get("UI-cellInRange");
-    cellTextures[enemy] = TextureManager::get("UI-enemyInRange");
-	cellTextures[active] = TextureManager::get("UI-activeUnit");
-    currentTexture = base;
+Box::Box(Texture *t, Vector2D<int> pos, Vector2D<int> size, Vector2D<int> bi,
+         Unit *go)
+    : GameObject(t, pos, size), boardIndex(bi), content(go) {
+  cellTextures[base] = t;
+  cellTextures[hover] = TextureManager::get("UI-cellHover");
+  cellTextures[movable] = TextureManager::get("UI-cellInRange");
+  cellTextures[enemy] = TextureManager::get("UI-enemyInRange");
+  cellTextures[active] = TextureManager::get("UI-activeUnit");
+  currentTexture = base;
 }
 
-Box::~Box()
-{
-    content = nullptr;
-}
+Box::~Box() { content = nullptr; }
 
 // Renders itself and its content
-void Box::render()
-{
-    cellTextures[currentTexture]->render(getRect(), texture_->getAnimation()[texture_->getFrame()]);
-    if (content != nullptr)
-    {
-        content->render();
-    }
+void Box::render() {
+  cellTextures[currentTexture]->render(
+      getRect(), texture_->getAnimation()[texture_->getFrame()]);
+  if (content != nullptr) {
+    content->render();
+  }
 }
 
-void Box::handleEvents(SDL_Event event)
-{
-	// Changes cell texture on mouse hover
-    SDL_Point p = {event.motion.x, event.motion.y};
-    if (SDL_PointInRect(&p, &getRect()))
-    {
-        currentTexture = hover;
-    }
-    else if (currentTexture == hover)
-    {
-        currentTexture = base;
-    }
+void Box::handleEvents(SDL_Event event) {
+  // Changes cell texture on mouse hover
+  SDL_Point p = {event.motion.x, event.motion.y};
+  if (SDL_PointInRect(&p, &getRect())) {
+    currentTexture = hover;
+  } else if (currentTexture == hover) {
+    currentTexture = base;
+  }
 }
 
 // ---------- Getters and Setters ----------
 
-Vector2D<int> Box::getIndex()
-{
-    return boardIndex;
-}
+Vector2D<int> Box::getIndex() { return boardIndex; }
 
-Unit *Box::getContent()
-{
-    return content;
-}
+Unit *Box::getContent() { return content; }
 
-void Box::setContent(Unit *object)
-{
-    content = object;
-}
+void Box::setContent(Unit *object) { content = object; }
 
-bool Box::isEmpty()
-{
-    return content == nullptr;
-}
+bool Box::isEmpty() { return content == nullptr; }
 
-int Box::getCurrentTexture() const
-{
-    return currentTexture;
-}
+int Box::getCurrentTexture() const { return currentTexture; }
 
-void Box::setCurrentTexture(int textureInd)
-{
-    currentTexture = textureInd;
-}
+void Box::setCurrentTexture(int textureInd) { currentTexture = textureInd; }
