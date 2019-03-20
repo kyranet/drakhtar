65c5a2a4753677ff090d2d138731b5e6f1fd8d23
onaranjoUCM <onaranjo@ucm.es>
2019-03-20 17:34:55 +0100

    fix: Clang formating redone

--- Drakhtar/Box.h
+++ Drakhtar/Box.h
@@ -6,34 +6,28 @@
 
 class Unit;
 
-class Box : public GameObject
-{
+class Box : public GameObject {
  protected:
-    Vector2D<int> boardIndex;
-    Unit *content;
-    int currentTexture;
-    Texture *cellTextures[5];
+  Vector2D<int> boardIndex;
+  Unit *content;
+  int currentTexture;
+  Texture *cellTextures[5];
 
  public:
-    Box(Texture *t, Vector2D<int> pos, Vector2D<int> size, Vector2D<int> boardIndex, Unit *go);
-    virtual ~Box();
+  Box(Texture *t, Vector2D<int> pos, Vector2D<int> size,
+      Vector2D<int> boardIndex, Unit *go);
+  virtual ~Box();
 
-    virtual void render();
-    virtual void handleEvents(SDL_Event event);
+  virtual void render();
+  virtual void handleEvents(SDL_Event event);
 
-	// Getters and Setters
-	bool isEmpty();
-	Vector2D<int> getIndex();
-    Unit *getContent();
-    void setContent(Unit *object);
-    int getCurrentTexture() const;
-    void setCurrentTexture(int textureInd);
+  // Getters and Setters
+  bool isEmpty();
+  Vector2D<int> getIndex();
+  Unit *getContent();
+  void setContent(Unit *object);
+  int getCurrentTexture() const;
+  void setCurrentTexture(int textureInd);
 
-    const enum textureInd {
-        base,
-        hover,
-        movable,
-		enemy,
-		active
-    };
+  const enum textureInd { base, hover, movable, enemy, active };
 };
