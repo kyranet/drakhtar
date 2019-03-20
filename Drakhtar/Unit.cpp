10d7ddc584b1000973d1b494a37b2b782c82da2f
onaranjoUCM <onaranjo@ucm.es>
2019-03-20 17:28:53 +0100

    fix: Clang formatting

--- Drakhtar/Unit.cpp
+++ Drakhtar/Unit.cpp
@@ -36,29 +36,19 @@ void Unit::moveTowards(Vector2D<int> pos)
     if (!moving_)
         return;
 
-    // TODO(Carlos): Do any logic here, interact with the Board
-    // TODO(Carlos): Maybe Unit->Team->Board?
+  // TODO(Carlos): Do any logic here, interact with the Board
+  // TODO(Carlos): Maybe Unit->Team->Board?
 }
 
-void Unit::setMoving(bool moving)
-{
-    moving_ = moving;
-}
+void Unit::setMoving(bool moving) { moving_ = moving; }
 
-void Unit::setMoved(bool moved)
-{
-    moved_ = moved;
-}
+void Unit::setMoved(bool moved) { moved_ = moved; }
 
-void Unit::setTeam(Team *team)
-{
-    team_ = team;
-}
+void Unit::setTeam(Team *team) { team_ = team; }
 
-void Unit::moveToBox(Box *newBox)
-{
-    // If it's not the unit's turn, cancel any action
-    // if (!moving_) return;
+void Unit::moveToBox(Box *newBox) {
+  // If it's not the unit's turn, cancel any action
+  // if (!moving_) return;
 
     box_->setContent(nullptr);
     pos_ = Vector2D<int>(newBox->getRect().x + newBox->getRect().w / 2, newBox->getRect().y + newBox->getRect().h / 2);
