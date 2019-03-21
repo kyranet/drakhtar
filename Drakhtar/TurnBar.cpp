36ea2cd996843483e9bef07333fd963ae313840c
Kyra <kyradiscord@gmail.com>
2019-03-21 12:50:41 +0100

    lint: Fixed all format errors

--- Drakhtar/TurnBar.cpp
+++ Drakhtar/TurnBar.cpp
@@ -2,127 +2,104 @@
 
 #include "TurnBar.h"
 
-
 TurnBar::TurnBar(list<Unit *> allyList, list<Unit *> enemyList)
-	: GameObject(TextureManager::get("UI-turnBar"),
-		Vector2D<int>(WIN_WIDTH - 215, WIN_HEIGHT - 30),
-		Vector2D<int>(400, 50))
-{
-	auto allyIt = allyList.begin();
-	auto enemyIt = enemyList.begin();
-	int unitCount = 0;
+    : GameObject(TextureManager::get("UI-turnBar"),
+                 Vector2D<int>(WIN_WIDTH - 215, WIN_HEIGHT - 30),
+                 Vector2D<int>(400, 50)) {
+  auto allyIt = allyList.begin();
+  auto enemyIt = enemyList.begin();
+  int unitCount = 0;
 
-	while (allyIt != allyList.end() || enemyIt != enemyList.end())
-	{
-		if (unitCount % 2 == 0 && allyIt != allyList.end())
-		{
-			unitTurnBar.push_back(*allyIt);
-			allyIt++;
-		}
-		else if (enemyIt != enemyList.end())
-		{
-			unitTurnBar.push_back(*enemyIt);
-			enemyIt++;
-		}
-		unitCount++;
-	}
+  while (allyIt != allyList.end() || enemyIt != enemyList.end()) {
+    if (unitCount % 2 == 0 && allyIt != allyList.end()) {
+      unitTurnBar.push_back(*allyIt);
+      allyIt++;
+    } else if (enemyIt != enemyList.end()) {
+      unitTurnBar.push_back(*enemyIt);
+      enemyIt++;
+    }
+    unitCount++;
+  }
 
-	visibleUnits.resize(visibleTurnBarSize);
-	auto listIt = unitTurnBar.begin();
-	for (int i = 0; i < visibleTurnBarSize; i++)
-	{
-		visibleUnits[i] = new GameObject((*listIt)->getTexture(),
-			Vector2D<int>(WIN_WIDTH - 432 + (i + 1) * 44, WIN_HEIGHT - 30),
-			Vector2D<int>(45, 45));
-		listIt++;
-	}
-	// example for testing sort
-	//auto e = allyList.begin();
-	//unitTurnBar.push_back(*e);
-	//unitTurnBar.push_back(*e);
-	//unitTurnBar.push_back(*e);
-	//unitTurnBar.push_back(*e);
-	//unitTurnBar.push_back(*e);
-	sort();
+  visibleUnits.resize(visibleTurnBarSize);
+  auto listIt = unitTurnBar.begin();
+  for (int i = 0; i < visibleTurnBarSize; i++) {
+    visibleUnits[i] = new GameObject(
+        (*listIt)->getTexture(),
+        Vector2D<int>(WIN_WIDTH - 432 + (i + 1) * 44, WIN_HEIGHT - 30),
+        Vector2D<int>(45, 45));
+    listIt++;
+  }
+  // example for testing sort
+  // auto e = allyList.begin();
+  // unitTurnBar.push_back(*e);
+  // unitTurnBar.push_back(*e);
+  // unitTurnBar.push_back(*e);
+  // unitTurnBar.push_back(*e);
+  // unitTurnBar.push_back(*e);
+  sort();
 }
 
-TurnBar::~TurnBar()
-{
-	for (auto unit : visibleUnits)
-		delete unit;
+TurnBar::~TurnBar() {
+  for (auto unit : visibleUnits) delete unit;
 }
-
 
 // takes out the unit in the front of the queue and puts it in the back
-void TurnBar::advanceTurn()
-{
-	Unit *frontUnit = unitTurnBar.front();
-	unitTurnBar.pop_front();
-	unitTurnBar.push_back(frontUnit);
-	updateVisibleUnits();
+void TurnBar::advanceTurn() {
+  Unit *frontUnit = unitTurnBar.front();
+  unitTurnBar.pop_front();
+  unitTurnBar.push_back(frontUnit);
+  updateVisibleUnits();
 }
 
-void TurnBar::sort()
-{
-	unitTurnBar.sort([](Unit * unit1, Unit * unit2) { return unit1->getSpeed() > unit2->getSpeed(); });
-	updateVisibleUnits();
+void TurnBar::sort() {
+  unitTurnBar.sort([](Unit *unit1, Unit *unit2) {
+    return unit1->getSpeed() > unit2->getSpeed();
+  });
+  updateVisibleUnits();
 }
 
-void TurnBar::eraseUnit(Unit * unit)
-{
-	bool deleted = false;
-	auto it = unitTurnBar.begin();
-	while (!deleted && it != unitTurnBar.end())
-	{
-		if ((*it) == unit)
-		{
-			unitTurnBar.erase(it);
-			deleted = true;
+void TurnBar::eraseUnit(Unit *unit) {
+  bool deleted = false;
+  auto it = unitTurnBar.begin();
+  while (!deleted && it != unitTurnBar.end()) {
+    if ((*it) == unit) {
+      unitTurnBar.erase(it);
+      deleted = true;
 
-			if (visibleTurnBarSize > unitTurnBar.size())
-				decreaseVisibleUnitsSize();
-		}
-		else
-		{
-			it++;
-		}
-	}
-	updateVisibleUnits();
+      if (visibleTurnBarSize > unitTurnBar.size()) decreaseVisibleUnitsSize();
+    } else {
+      it++;
+    }
+  }
+  updateVisibleUnits();
 }
 
-void TurnBar::render() const
-{
-	GameObject::render();
-	for (auto unit : visibleUnits)
-		unit->render();
+void TurnBar::render() const {
+  GameObject::render();
+  for (auto unit : visibleUnits) unit->render();
 }
 
-void TurnBar::handleEvents(SDL_Event event)
-{
-	if (event.type == SDL_KEYDOWN)
-	{
-		switch (event.key.keysym.sym)
-		{
-		case SDLK_t:
-			advanceTurn();
-			break;
-		}
-	}
+void TurnBar::handleEvents(SDL_Event event) {
+  if (event.type == SDL_KEYDOWN) {
+    switch (event.key.keysym.sym) {
+      case SDLK_t:
+        advanceTurn();
+        break;
+    }
+  }
 }
 
-void TurnBar::updateVisibleUnits()
-{
-	auto listIt = unitTurnBar.begin();
-	for (int i = 0; i < visibleTurnBarSize; i++)
-	{
-		visibleUnits[i]->setTexture((*listIt)->getTexture());
-		listIt++;
-	}
+void TurnBar::updateVisibleUnits() {
+  auto listIt = unitTurnBar.begin();
+  for (int i = 0; i < visibleTurnBarSize; i++) {
+    visibleUnits[i]->setTexture((*listIt)->getTexture());
+    listIt++;
+  }
 }
 
-void TurnBar::decreaseVisibleUnitsSize()
-{
-	visibleTurnBarSize--;
-	delete visibleUnits[visibleTurnBarSize];
-	visibleUnits.resize(visibleTurnBarSize);
+void TurnBar::decreaseVisibleUnitsSize() {
+  visibleTurnBarSize--;
+  delete visibleUnits[visibleTurnBarSize];
+  visibleUnits.resize(visibleTurnBarSize);
 }
--- Drakhtar/TurnBar.h
+++ Drakhtar/TurnBar.h
@@ -1,29 +1,29 @@
 // Copyright 2019 the Drakhtar authors. All rights reserved. MIT license.
 
 #pragma once
-#include <queue>
 #include <list>
+#include <queue>
 #include <vector>
-#include "Unit.h"
 #include "TextureManager.h"
+#include "Unit.h"
 
-class TurnBar : public GameObject
-{
-private:
-	list<Unit *> unitTurnBar;
-	int visibleTurnBarSize = 9;
-	vector<GameObject *> visibleUnits;
+class TurnBar : public GameObject {
+ private:
+  list<Unit *> unitTurnBar;
+  int visibleTurnBarSize = 9;
+  vector<GameObject *> visibleUnits;
 
-public:
-	// constructor intercalates units from each team list into the turn bar (ally -> enemy -> ally -> etc)
-	TurnBar(list<Unit *> allyList, list<Unit *> enemyList);
-	virtual ~TurnBar();
-	Unit *getFrontUnit() { return unitTurnBar.front(); }
-	void advanceTurn();
-	void sort();
-	void eraseUnit(Unit* unit);
-	virtual void render() const;
-	virtual void handleEvents(SDL_Event event);
-	void updateVisibleUnits();
-	void decreaseVisibleUnitsSize();
+ public:
+  // constructor intercalates units from each team list into the turn bar (ally
+  // -> enemy -> ally -> etc)
+  TurnBar(list<Unit *> allyList, list<Unit *> enemyList);
+  virtual ~TurnBar();
+  Unit *getFrontUnit() { return unitTurnBar.front(); }
+  void advanceTurn();
+  void sort();
+  void eraseUnit(Unit *unit);
+  virtual void render() const;
+  virtual void handleEvents(SDL_Event event);
+  void updateVisibleUnits();
+  void decreaseVisibleUnitsSize();
 };
