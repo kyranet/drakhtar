7e6a46eb17de56d2256ee66c4d586bd9d5fa5d91
CarlosMGM <gonzalem@ucm.es>
2019-04-02 17:50:46 +0200

    added buyUnits() and text showing current money

--- Drakhtar/Scenes/RecruitScene.cpp
+++ Drakhtar/Scenes/RecruitScene.cpp
@@ -3,45 +3,35 @@
 #include "GameObjects/GameObject.h"
 #include "Utils/Constants.h"
 #include "EventListeners/UnitSelectorController.h"
-
-
-void RecruitScene::addUnit(string textureName, int position)
-{
-
-  auto rect = unitBar->getRect();
-
-  position -= 3;
-
-  auto unit = new GameObject(this,
-    TextureManager::get(textureName),
-    Vector2D<int>(WIN_WIDTH / 2 + rect.w * position / 6,
-      WIN_HEIGHT - WIN_HEIGHT / 13),
-    Vector2D<int>(WIN_HEIGHT / 6,
-      WIN_HEIGHT / 6));
-  controller->addUnitToStore(unit);
-
-  addGameObject(unit);
-
-  unit = new GameObject(this,
-    TextureManager::get(textureName),
-    Vector2D<int>(WIN_WIDTH / 4,
-      WIN_HEIGHT - WIN_HEIGHT / 2),
-    Vector2D<int>(WIN_HEIGHT / 3,
-      WIN_HEIGHT / 3));
-
-  controller->addBigUnit(unit);
-
-  addGameObject(unit);
-}
+#include "Utils/PlayerData.h"
+#include "GameObjects/Text.h"
+#include "Managers/FontManager.h"
+#include "Utils/Vector2D.h"
 
 void RecruitScene::preload()
 {
+	PlayerData::getInstance()->addMoney(100);
+
+	costs_["Soldier"] = 10;
+	costs_["Archer"] = 10;
+	costs_["Mage"] = 13;
+	costs_["Knight"] = 20;
+	costs_["Monster"] = 18;
+
 	const auto background =
 		new GameObject(this, TextureManager::get("Maps-SecondBattle"),
 			Vector2D<int>(WIN_WIDTH / 2, WIN_HEIGHT / 2),
 			Vector2D<int>(WIN_WIDTH, WIN_HEIGHT));
 
-	unitBar =
+
+
+	moneyText_ = new Text(this, FontManager::get("Retron2000"),
+		Vector2D<int>(WIN_WIDTH / 15 , WIN_HEIGHT / 10),
+		{ 0,0,0,0 },
+		moneyToString(),
+		WIN_WIDTH);
+
+	unitBar_ =
 		new GameObject(this, TextureManager::get("UI-turnBar"),
 			Vector2D<int>(WIN_WIDTH / 2,
 				WIN_HEIGHT - WIN_HEIGHT / 13),
@@ -56,16 +46,61 @@ void RecruitScene::preload()
 
 
   addGameObject(background);
-  addGameObject(unitBar);
+  addGameObject(moneyText_);
+  addGameObject(unitBar_);
   addGameObject(circle);
 
-  controller = new UnitSelectorController(unitBar);
+  controller_ = new UnitSelectorController(unitBar_);
 
-  unitBar->addEventListener(controller);
+  unitBar_->addEventListener(controller_);
 
   addUnit("Units-BlueSoldier", 1);
   addUnit("Units-BlueArcher", 2);
   addUnit("Units-BlueMage", 3);
   addUnit("Units-BlueKnight", 4);
   addUnit("Units-BlueMonster", 5);
+}
+
+void RecruitScene::buyUnits(string type, int quantity)
+{
+	if (costs_[type] * quantity <= PlayerData::getInstance()->getMoney()) {
+		PlayerData::getInstance()->addUnits(type, quantity);
+		PlayerData::getInstance()->loseMoney(costs_[type] * quantity);
+		moneyText_->setText(moneyToString());
+	}
+}
+
+
+void RecruitScene::addUnit(string textureName, int position)
+{
+
+	auto rect = unitBar_->getRect();
+
+	position -= 3;
+
+	auto unit = new GameObject(this,
+		TextureManager::get(textureName),
+		Vector2D<int>(WIN_WIDTH / 2 + rect.w * position / 6,
+			WIN_HEIGHT - WIN_HEIGHT / 13),
+		Vector2D<int>(WIN_HEIGHT / 6,
+			WIN_HEIGHT / 6));
+	controller_->addUnitToStore(unit);
+
+	addGameObject(unit);
+
+	unit = new GameObject(this,
+		TextureManager::get(textureName),
+		Vector2D<int>(WIN_WIDTH / 4,
+			WIN_HEIGHT - WIN_HEIGHT / 2),
+		Vector2D<int>(WIN_HEIGHT / 3,
+			WIN_HEIGHT / 3));
+
+	controller_->addBigUnit(unit);
+
+	addGameObject(unit);
+}
+
+string RecruitScene::moneyToString()
+{
+	return "Money: " + to_string(PlayerData::getInstance()->getMoney());
 }
--- Drakhtar/Scenes/RecruitScene.h
+++ Drakhtar/Scenes/RecruitScene.h
@@ -4,20 +4,26 @@
 
 #include "Scene.h"
 #include <string>
+#include <map>
 
 using namespace std;
 
 class GameObject;
 class UnitSelectorController;
+class Text;
 
 class RecruitScene : public Scene {
 private:
-	GameObject * unitBar = nullptr;
-  UnitSelectorController * controller = nullptr;
-  
+  GameObject * unitBar_ = nullptr;
+  UnitSelectorController * controller_ = nullptr;
+  map<string, int> costs_;
+  Text * moneyText_;
 
   void addUnit(string textureName, int position);
+  string moneyToString();
+
 public:
 	//virtual ~RecruitScene();
 	void preload() override;
+	void buyUnits(string type, int quantity);
 };
