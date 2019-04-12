ffc1830ec94617a2400807f851cb18788fbfe644
Dankgard <dangar18@ucm.es>
2019-04-12 18:11:34 +0200

    feat: Working damage bar without animations

--- Drakhtar/GameObjects/HealthBar.cpp
+++ Drakhtar/GameObjects/HealthBar.cpp
@@ -56,9 +56,9 @@ void HealthBar::takeDamage(int damage)
 	lifeBar->setSize(Vector2D<int>(originalWidth * lifeProportion, lifeBar->getRect().h));
 	lifeBar->setPosition(Vector2D<int>(oldX + (lifeBar->getRect().w / 2), oldY + (lifeBar->getRect().h / 2)));
 
-	damageBar->setPosition(Vector2D<int>(damageBar->getRect().x + lifeBar->getRect().w, damageBar->getRect().y));
 	double damageProportion = (double)damage / maxHealth;
 	damageBar->setSize(Vector2D<int>(originalWidth * damageProportion, damageBar->getRect().h));
+	damageBar->setPosition(Vector2D<int>(lifeBar->getRect().x + (lifeBar->getRect().w) + (damageBar->getRect().w/2), damageBar->getRect().y + (lifeBar->getRect().h/2)));
 	damageBar->setActive(true);
 	// damageAnimationPlaying = true;
 }
