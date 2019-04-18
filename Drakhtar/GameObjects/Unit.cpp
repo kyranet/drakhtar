f92f4fc4a18461d362046ca08e61b92966ccf472
onaranjoUCM <37249723+onaranjoUCM@users.noreply.github.com>
2019-04-18 20:08:33 +0200

    Defence and negative health (#94)
    * fix: Negative health units
    
    * fix: Unit not ending turn after move+attack
    
    * feat: Defense reworked to be percent
    
    * lint

--- Drakhtar/GameObjects/Unit.cpp
+++ Drakhtar/GameObjects/Unit.cpp
@@ -1,15 +1,17 @@
 // Copyright 2019 the Drakhtar authors. All rights reserved. MIT license.
 
 #include "Unit.h"
+
+#include <algorithm>
+
 #include "../Managers/FontManager.h"
 #include "../Structures/Team.h"
 #include "../Utils/Vector2D.h"
 #include "Box.h"
 #include "HealthBar.h"
 #include "Text.h"
-#include <algorithm>
 
-Unit::Unit(Scene *scene, Texture *texture, Box *box, UnitStats stats,
+Unit::Unit(Scene* scene, Texture* texture, Box* box, UnitStats stats,
            const std::string& type)
     : GameObject(scene, texture,
                  Vector2D<int>(box->getRect().x + box->getRect().w / 2,
@@ -50,28 +52,31 @@ Unit::~Unit() {
   }
 }
 
-void Unit::moveToBox(Box *newBox) {
+void Unit::moveToBox(Box* newBox) {
   box_->setContent(nullptr);
   setPosition(Vector2D<int>(newBox->getRect().x + newBox->getRect().w / 2,
                             newBox->getRect().y + newBox->getRect().h / 2));
   box_ = newBox;
   newBox->setContent(this);
   const auto rect = box_->getRect();
-  healthText_->setPosition({rect.x + rect.w / 2 + rect.w / 14, rect.y - rect.h / 3});
-  healthBar_->moveBar(
-      Vector2D<int>(rect.x + rect.w / 2, rect.y - rect.h / 3));
+  healthText_->setPosition(
+      {rect.x + rect.w / 2 + rect.w / 14, rect.y - rect.h / 3});
+  healthBar_->moveBar(Vector2D<int>(rect.x + rect.w / 2, rect.y - rect.h / 3));
   setMoved(true);
   setMoving(false);
 }
 
 int Unit::loseHealth(int enemyAttack) {
-  enemyAttack = std::max(enemyAttack - getDefense(), 1);
+  enemyAttack = enemyAttack * (1 - static_cast<float>(getDefense()) / 100);
+
   health_ = std::max(health_ - enemyAttack, 0);
-  stats_.health -= enemyAttack;
+  stats_.health = std::max(stats_.health - enemyAttack, 0);
+
   healthText_->setText(healthToString());
-  const SDL_Color textColor = { 255, 255, 255, 0 };
+  const SDL_Color textColor = {255, 255, 255, 0};
   healthText_->setColor(textColor);
   healthBar_->takeDamage(getStats().health);
+
   return enemyAttack;
 }
 
@@ -87,7 +92,7 @@ void Unit::onSelect() { setMoving(true); }
 
 void Unit::onDeselect() { setMoving(false); }
 
-void Unit::attack(Unit *enemy, const bool counter) {
+void Unit::attack(Unit* enemy, const bool counter) {
   enemy->loseHealth(this->getStats().attack);
 
   // If the attack is not a counter and the enemy is
