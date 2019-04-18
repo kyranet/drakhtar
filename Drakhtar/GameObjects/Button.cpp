635eb4007047de7d6c7a1b99fecce3b263008655
Alejandro Placer <aplazer@ucm.es>
2019-04-18 14:43:23 +0200

    Fix: button text size error

--- Drakhtar/GameObjects/Button.cpp
+++ Drakhtar/GameObjects/Button.cpp
@@ -1,11 +1,13 @@
 // Copyright 2019 the Drakhtar authors. All rights reserved. MIT license.
 
 #include "Button.h"
+
 #include <utility>
-#include "Managers/Input.h"
-#include "Text.h"
+
 #include "../Structures/Font.h"
 #include "Managers/FontManager.h"
+#include "Managers/Input.h"
+#include "Text.h"
 
 Button::Button(Scene* scene, Texture* texture, const Vector2D<int>& pos,
                const Vector2D<int>& size, std::function<void()> callback,
@@ -13,23 +15,18 @@ Button::Button(Scene* scene, Texture* texture, const Vector2D<int>& pos,
     : GameObject(scene, texture, pos, size), callback_(std::move(callback)) {
   spriteText_ = text;
 
-  const SDL_Color textColor = {0, 0, 0, 255};
+  const SDL_Color textColor = {225, 225, 225, 255};
 
-  const auto buttonText_ = new Text(
-      scene, FontManager::get(fontFile),
-               Vector2D<int>(this->getRect().x + this->getRect().w / 10,
-                             this->getRect().y + this->getRect().h / 200),
-      textColor, spriteText_, this->getRect().w*0.95);
+  const auto buttonText_ =
+      new Text(scene, FontManager::get(fontFile),
+               Vector2D<int>(pos.getX() + WIN_WIDTH / 20,
+                             this->getRect().y + WIN_HEIGHT / 200),
+               textColor, spriteText_, this->getRect().w * 0.95);
 
   buttonText_->setColor(textColor);
-  buttonText_->setText(spriteText_, textColor, this->getRect().w * 0.75);
-  buttonText_->setPosition(
-      Vector2D<int>(this->getRect().x + buttonText_->getRect().w/ 50,
-                    this->getRect().y - buttonText_->getRect().h/ 50));
-  buttonText_->setSize({100, 70});
-  //buttonText_->setSize(
-  //    Vector2D<int>(this->getRect().x + buttonText_->getRect().w / 10,
-    //                this->getRect().y - buttonText_->getRect().h / 10));
+  buttonText_->setText(spriteText_, textColor, this->getRect().w * 0.95);
+  buttonText_->setPosition(Vector2D<int>(pos.getX(), pos.getY()));
+  buttonText_->setSize(Vector2D<int>(size.getX()*0.65, size.getY()*0.8));
 
   addChild(buttonText_);
 }
@@ -38,7 +35,7 @@ void Button::update() {
   GameObject::update();
 
   if ((Input::isMouseButtonDown(MouseKey::LEFT) ||
-      Input::isMouseButtonDown(MouseKey::RIGHT)) &&
+       Input::isMouseButtonDown(MouseKey::RIGHT)) &&
       reinterpret_cast<Button*>(Input::screenMouseToRay()) == this) {
     callback_();
   }
