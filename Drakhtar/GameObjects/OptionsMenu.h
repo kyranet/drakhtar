2a536a81a5363ccd943325af02217f61795e4688
Arya Stark <miguelye@ucm.es>
2019-04-19 14:43:37 +0200

    what is this

--- Drakhtar/GameObjects/OptionsMenu.h
+++
@@ -1,8 +0,0 @@
-// Copyright 2019 the Drakhtar authors. All rights reserved. MIT license.
-#pragma once
-
-#include "GameObject.h"
-class OptionsMenu : public GameObject {
- public:
-  explicit OptionsMenu(Scene* scene);
-};
