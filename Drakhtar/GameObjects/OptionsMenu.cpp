4dedffe567bc5901cf203a14d1524d6c2651cb9e
Arya Stark <miguelye@ucm.es>
2019-04-22 00:28:25 +0200

    fix: texts now being seen

--- Drakhtar/GameObjects/OptionsMenu.cpp
+++ Drakhtar/GameObjects/OptionsMenu.cpp
@@ -44,42 +44,40 @@ OptionsMenu::OptionsMenu(Scene* scene) : GameObject(scene, nullptr) {
                   Vector2D<int>(WIN_WIDTH / 1.85, WIN_HEIGHT / 2.15),
                   Vector2D<int>(static_cast<int>(WIN_WIDTH / 24),
                                 static_cast<int>(WIN_HEIGHT / 20)),
-                  [this]() { SDLAudioManager::getInstance()->setMute(false); });
-   const auto soundButton =
-       new Button(scene_, TextureManager::get("UI-MuteSound"),
-                  Vector2D<int>(WIN_WIDTH / 2.15, WIN_HEIGHT / 2.15),
-                  Vector2D<int>(static_cast<int>(WIN_WIDTH / 22),
-                                static_cast<int>(WIN_HEIGHT / 20)),
-                  [this]() { SDLAudioManager::getInstance()->setMute(true);
-   });*/
+                  [this]() { SDLAudioManager::getInstance()->setMute(false);
+     });*/
+  const auto soundButton =
+      new GameObject(scene_, TextureManager::get("UI-ActiveSound"),
+                     Vector2D<int>(WIN_WIDTH / 2, WIN_HEIGHT / 2.15),
+                     Vector2D<int>(static_cast<int>(WIN_WIDTH / 22),
+                                   static_cast<int>(WIN_HEIGHT / 20)));
   const auto moreSoundBox =
       new Button(scene_, TextureManager::get("Quantity-Button"),
-                 Vector2D<int>(WIN_WIDTH / 2.3, WIN_HEIGHT / 2.15),
-                 Vector2D<int>(static_cast<int>(WIN_WIDTH / 18),
-                               static_cast<int>(WIN_HEIGHT / 10)),
+                 Vector2D<int>(WIN_WIDTH / 1.75, WIN_HEIGHT / 2.15),
+                 Vector2D<int>(static_cast<int>(WIN_WIDTH / 15),
+                               static_cast<int>(WIN_HEIGHT / 8)),
                  [this]() { highervolume(); });
   const auto LessSoundBox =
       new Button(scene_, TextureManager::get("Quantity-Button"),
-                 Vector2D<int>(WIN_WIDTH / 1.75, WIN_HEIGHT / 2.15),
-                 Vector2D<int>(static_cast<int>(WIN_WIDTH / 18),
-                               static_cast<int>(WIN_HEIGHT / 10)),
+                 Vector2D<int>(WIN_WIDTH / 2.35, WIN_HEIGHT / 2.15),
+                 Vector2D<int>(static_cast<int>(WIN_WIDTH / 15),
+                               static_cast<int>(WIN_HEIGHT / 8)),
                  [this]() { lowervolume(); });
 
   const auto plusText =
       new Text(scene_, FontManager::get("StatsFont"),
-               Vector2D<int>(WIN_WIDTH / 1.75, WIN_HEIGHT / 2.15), {0, 0, 0, 0},
-               " mas dd", 10000);
+               Vector2D<int>(WIN_WIDTH / 1.75, WIN_HEIGHT / 2.16), {0, 0, 0, 0},
+               "+", 10000);
   const auto minusText =
-      new Text(scene_, FontManager::get("Retron2000"),
-               Vector2D<int>(WIN_WIDTH / 1.75, WIN_HEIGHT / 2.15), {0, 0, 0, 0},
+      new Text(scene_, FontManager::get("StatsFont"),
+               Vector2D<int>(WIN_WIDTH /2.35, WIN_HEIGHT / 2.16), {0, 0, 0, 0},
                "-", 10000);
 
   addChild(panel);
-  scene_->addGameObject(plusText);
+  moreSoundBox->addChild(plusText);
+  LessSoundBox->addChild(minusText);
   addChild(returnButton);
   addChild(moreSoundBox);
   addChild(LessSoundBox);
-
-  // addChild(noSoundButton);
-  // addChild(soundButton);
+  addChild(soundButton);
 }
