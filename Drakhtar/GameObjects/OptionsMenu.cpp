adb47e1fe90e191cbfa7676dfcc33dcdb9bd3a3f
Arya Stark <miguelye@ucm.es>
2019-04-22 19:29:21 +0200

    LINTING

--- Drakhtar/GameObjects/OptionsMenu.cpp
+++ Drakhtar/GameObjects/OptionsMenu.cpp
@@ -59,7 +59,7 @@ OptionsMenu::OptionsMenu(Scene* scene) : GameObject(scene, nullptr) {
       Vector2D<int>(static_cast<int>(WIN_WIDTH / 15),
                     static_cast<int>(WIN_HEIGHT / 8)),
       [this]() {
-        SDLAudioManager::getInstance()->getDefault() ? highervolume() : 0;
+        SDLAudioManager::getInstance()->getDefault() ? highervolume() : NULL;
       },
       "+", "StatsFont");
   const auto LessSoundBox = new Button(
@@ -68,7 +68,7 @@ OptionsMenu::OptionsMenu(Scene* scene) : GameObject(scene, nullptr) {
       Vector2D<int>(static_cast<int>(WIN_WIDTH / 15),
                     static_cast<int>(WIN_HEIGHT / 8)),
       [this]() {
-        SDLAudioManager::getInstance()->getDefault() ? lowervolume() : 0;
+        SDLAudioManager::getInstance()->getDefault() ? lowervolume() : NULL;
       },
       "-", "StatsFont");
 
