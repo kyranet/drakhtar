e510a3a6f962b03494cfdd1b258a12af6d3e5620
Arya Stark <miguelye@ucm.es>
2019-04-22 19:10:51 +0200

    fix: buttons working as they should(finally dumbass)

--- Drakhtar/GameObjects/OptionsMenu.cpp
+++ Drakhtar/GameObjects/OptionsMenu.cpp
@@ -35,15 +35,15 @@ OptionsMenu::OptionsMenu(Scene* scene) : GameObject(scene, nullptr) {
                      Vector2D<int>(static_cast<int>(WIN_WIDTH / 4),
                                    static_cast<int>(WIN_HEIGHT / 3)));
   const auto returnButton = new Button(
-      scene_, TextureManager::get("Button-Exit"),
+      scene_, TextureManager::get("Vanilla-Button"),
       Vector2D<int>(WIN_WIDTH / 2, WIN_HEIGHT / 1.65),
-      Vector2D<int>(static_cast<int>(WIN_WIDTH / 14),
-                    static_cast<int>(WIN_HEIGHT / 12)),
-      [this]() { destroy(); }, "Return", "StatsFont");
+      Vector2D<int>(static_cast<int>(WIN_WIDTH / 10),
+                    static_cast<int>(WIN_HEIGHT / 8)),
+      [this]() { destroy(); }, "Return", "ButtonFont");
   const auto soundIcon =
       new GameObject(scene_, TextureManager::get("UI-ActiveSound"),
                      Vector2D<int>(WIN_WIDTH / 2, WIN_HEIGHT / 2.25),
-                     Vector2D<int>(static_cast<int>(WIN_WIDTH / 22),
+                     Vector2D<int>(static_cast<int>(WIN_WIDTH / 25),
                                    static_cast<int>(WIN_HEIGHT / 20)));
   const auto moreSoundBox = new Button(
       scene_, TextureManager::get("Quantity-Button"),
@@ -51,27 +51,19 @@ OptionsMenu::OptionsMenu(Scene* scene) : GameObject(scene, nullptr) {
       Vector2D<int>(static_cast<int>(WIN_WIDTH / 15),
                     static_cast<int>(WIN_HEIGHT / 8)),
       [this]() {
-        SDLAudioManager::getInstance()->getDefault() ? NULL : highervolume();
+        SDLAudioManager::getInstance()->getDefault() ? highervolume() : 0;
       },
-      "More Sound", "StatsFont");
+      "+", "StatsFont");
   const auto LessSoundBox = new Button(
       scene_, TextureManager::get("Quantity-Button"),
       Vector2D<int>(WIN_WIDTH / 2.35, WIN_HEIGHT / 2.25),
       Vector2D<int>(static_cast<int>(WIN_WIDTH / 15),
                     static_cast<int>(WIN_HEIGHT / 8)),
       [this]() {
-        SDLAudioManager::getInstance()->getDefault() ? NULL : lowervolume();
+        SDLAudioManager::getInstance()->getDefault() ? lowervolume() : 0;
       },
-      "Less Sound", "StatsFont");
+      "-", "StatsFont");
 
-  const auto plusText =
-      new Text(scene_, FontManager::get("StatsFont"),
-               Vector2D<int>(WIN_WIDTH / 1.75, WIN_HEIGHT / 2.26), {0, 0, 0, 0},
-               "+", 10000);
-  const auto minusText =
-      new Text(scene_, FontManager::get("StatsFont"),
-               Vector2D<int>(WIN_WIDTH / 2.35, WIN_HEIGHT / 2.26), {0, 0, 0, 0},
-               "-", 10000);
   const auto DefaultText =
       new Text(scene_, FontManager::get("StatsFont"),
                Vector2D<int>(WIN_WIDTH / 1.9, WIN_HEIGHT / 1.9), {0, 0, 0, 0},
@@ -90,11 +82,9 @@ OptionsMenu::OptionsMenu(Scene* scene) : GameObject(scene, nullptr) {
         SDLAudioManager::getInstance()->setDefault(
             !SDLAudioManager::getInstance()->getDefault());
       },
-      "Default", "StatsFont");
+      " ", "ButtonFont");
 
   addChild(panel);
-  moreSoundBox->addChild(plusText);
-  LessSoundBox->addChild(minusText);
   addChild(returnButton);
   addChild(moreSoundBox);
   addChild(LessSoundBox);
@@ -111,7 +101,7 @@ OptionsMenu::~OptionsMenu() {
 
 void OptionsMenu::render() const {
   GameObject::render();
-  if (SDLAudioManager::getInstance()->getDefault()) {
+  if (!SDLAudioManager::getInstance()->getDefault()) {
     greenTick->render(greenTick->getRect());
   }
 }
