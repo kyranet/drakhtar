22c101e1f939a3c3e2151296d4c36c5c217976d9
GonzaPM7 <37514571+GonzaPM7@users.noreply.github.com>
2019-05-07 17:31:37 +0200

    Background transition (#133)
    * add: Transition_3
    
    * add: Transition_4
    
    * add: MapTransition5
    
    * end: MapTransition5

--- Drakhtar/Structures/Game.cpp
+++ Drakhtar/Structures/Game.cpp
@@ -247,11 +247,12 @@ void Game::load() {
   textures->add("Transition-Map1", "../images/Maps/TestMap.png", 1, 1);
   textures->add("Transition-Map2", "../images/Transition/Transition_2.png", 1,
                 1);
-  textures->add("Transition-Map3", "../images/Transition/GuerrillaSpecial.png",
-                1, 1);
-  textures->add("Transition-Map4", "../images/Transition/GuerrillaSpecial.png",
-                1, 1);
-  textures->add("Transition-Map5", "../images/Transition/GuerrillaSpecial.png",
+  textures->add("Transition-Map3", "../images/Transition/Transition_3.png", 4,
+                1)
+  ->addAnimation("default", {0, 1, 2, 3}, 1);
+  textures->add("Transition-Map4", "../images/Transition/Transition_4.png",
+                4, 1);
+  textures->add("Transition-Map5", "../images/Transition/Transition_5.png",
                 1, 1);
   textures->add("Transition-Menu", "../images/Transition/Transition_1.png", 4,
                 1);
