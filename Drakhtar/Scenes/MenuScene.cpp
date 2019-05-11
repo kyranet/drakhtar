2967e18bc2b81361453eee5e8905b4d792b750fd
GonzaPM7 <37514571+GonzaPM7@users.noreply.github.com>
2019-05-11 16:30:43 +0200

    Font-NewMenuDesign (#144)
    * add: new font
    
    * add: new menu design
    
    * fix:lint
    
    * fix:size button
    
    * adjust:pos button
    
    * fix:build
    
    * fix:build*2

--- Drakhtar/Scenes/MenuScene.cpp
+++ Drakhtar/Scenes/MenuScene.cpp
@@ -24,21 +24,28 @@ void MenuScene::preload() {
       new GameObject(this, TextureManager::get("Transition-Menu"),
                      Vector2D<int>(WIN_WIDTH / 2, WIN_HEIGHT / 2),
                      Vector2D<int>(WIN_WIDTH, WIN_HEIGHT));
+  const auto vg = new GameObject(this, TextureManager::get("Logos-VG"),
+                                 Vector2D<int>(WIN_WIDTH - 50, WIN_HEIGHT - 40),
+                                 Vector2D<int>(100, 75));
+  const auto logo =
+      new GameObject(this, TextureManager::get("Logos-logo"),
+                     Vector2D<int>(WIN_WIDTH / 2, (WIN_HEIGHT / 2) - 150),
+                     Vector2D<int>(800, 400));
   const auto play = new Button(
       this, TextureManager::get("Vanilla-Button"),
-      Vector2D<int>(WIN_WIDTH / 2, WIN_HEIGHT / 2 - WIN_HEIGHT / 18),
-      Vector2D<int>(static_cast<int>(floor(WIN_WIDTH / 6.5)), WIN_HEIGHT / 11),
+      Vector2D<int>(WIN_WIDTH / 2, WIN_HEIGHT / 2 + WIN_HEIGHT / 60),
+      Vector2D<int>(static_cast<int>(floor(WIN_WIDTH / 5.5)), WIN_HEIGHT / 10),
       buttonPlay, "Play", "ButtonFont");
   const auto options = new Button(
       this, TextureManager::get("Vanilla-Button"),
-      Vector2D<int>(WIN_WIDTH / 2, WIN_HEIGHT / 2 + WIN_HEIGHT / 18),
-      Vector2D<int>(static_cast<int>(floor(WIN_WIDTH / 6.5)), WIN_HEIGHT / 11),
+      Vector2D<int>(WIN_WIDTH / 2, WIN_HEIGHT / 2 + (WIN_HEIGHT / 60 + 80)),
+      Vector2D<int>(static_cast<int>(floor(WIN_WIDTH / 5.5)), WIN_HEIGHT / 10),
       [this]() { this->addGameObject(new OptionsMenu(this)); }, "Options",
       "ButtonFont");
   const auto exitButton = new Button(
       this, TextureManager::get("Vanilla-Button"),
-      Vector2D<int>(WIN_WIDTH / 2, WIN_HEIGHT / 2 + 2 * (WIN_HEIGHT / 13)),
-      Vector2D<int>(static_cast<int>(floor(WIN_WIDTH / 6.5)), WIN_HEIGHT / 11),
+      Vector2D<int>(WIN_WIDTH / 2, WIN_HEIGHT / 2 + (WIN_HEIGHT / 60 + 160)),
+      Vector2D<int>(static_cast<int>(floor(WIN_WIDTH / 5.5)), WIN_HEIGHT / 10),
       [this]() { finish(true); }, "Exit Game", "ButtonFont");
 
   auto audio = SDLAudioManager::getInstance();
@@ -47,6 +54,8 @@ void MenuScene::preload() {
   audio->playMusic(0, -1);
   if (audio->getDefault()) audio->setMusicVolume(30);
   addGameObject(background);
+  addGameObject(vg);
+  addGameObject(logo);
   addGameObject(play);
   addGameObject(options);
   addGameObject(exitButton);
