f25004dc5b043e6ea238e00300603b4303270e3a
Kyra <kyradiscord@gmail.com>
2019-05-15 16:57:38 +0200

    lint: Resolved max-len warning

--- Drakhtar/Managers/State.cpp
+++ Drakhtar/Managers/State.cpp
@@ -90,8 +90,9 @@ bool State::move(const Vector2D<byte>& from, const Vector2D<byte>& to) {
 
   State::UnitState state(
       previous.unit_, previous.team_, to, previous.attack_, previous.health_,
-      previous.minimumAttack_, previous.defense_, previous.maxHealth_, previous.attackRange_,
-      previous.moveRange_, previous.speed_, previous.prize_, previous.counterAttacked_);
+      previous.minimumAttack_, previous.defense_, previous.maxHealth_,
+      previous.attackRange_, previous.moveRange_, previous.speed_,
+      previous.prize_, previous.counterAttacked_);
 
   setAt(to, state);
   return true;
